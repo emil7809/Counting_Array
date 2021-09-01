b1b463122dda0aff3e466d98736f65dfd6f5b9b9
emily <emil7809@gmail.com>
2021-09-01 12:57:07 +0200

    that didnt work

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 "use strict";
 
-const max = 10;
-let iterator = 0;
+// const max = 10;
+// let iterator = 0;
+
+// loop();
+
+// function loop(){
+//     iterator++;
+
+//     if (iterator <= max) {
+//         console.log(iterator);
+//         setTimeout(loop, 300);
+//     }
+// }
+
+const count = ['1','2','3','4','5','6','7','8','9','10'];
+let newarr = count[0];
 
 loop();
 
-function loop(){
-    iterator++;
-
-    if (iterator <= max) {
-        console.log(iterator);
-        setTimeout(loop, 300);
-    }
+function loop() {
+    newarr++;
+    console.log(newarr);
+    setTimeout(loop, 500);
 }
